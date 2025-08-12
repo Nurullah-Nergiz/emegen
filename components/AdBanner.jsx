8e65c5c3934e757f53cfb442c872d343753beced
Nurullah NERGİZ <nurullah.devs@gmail.com>
2025-08-12 21:25:19 +0300

    Refactor AdBanner component to comment out unused ad code and improve image display structure

--- components/AdBanner.jsx
+++ components/AdBanner.jsx
@@ -3,46 +3,36 @@
 import { useEffect } from "react";
 
 export const Ad = () => {
-   useEffect(() => {
-      if (typeof window !== "undefined" && window.adsbygoogle) {
-         try {
-            (window.adsbygoogle = window.adsbygoogle || []).push({});
-            console.log(
-               "file: AdBanner.jsx:10 => window.adsbygoogle=>",
-               window.adsbygoogle
-            );
-         } catch (e) {
-            console.error("Adsense error:", e);
-         }
-      }
-   }, []);
+   // useEffect(() => {
+   //    if (typeof window !== "undefined" && window.adsbygoogle) {
+   //       try {
+   //          (window.adsbygoogle = window.adsbygoogle || []).push({});
+   //          // console.log(
+   //          //    "file: AdBanner.jsx:10 => window.adsbygoogle=>",
+   //          //    window.adsbygoogle
+   //          // );
+   //       } catch (e) {
+   //          // console.error("Adsense error:", e);
+   //       }
+   //    }
+   // }, []);
 
    return (
       <>
-         <ins
+         {/* <ins
             className="adsbygoogle block min-h-52 !bg-secondary  main !p-0 relative overflow-hidden"
             // style="display:block"
             data-ad-client="ca-pub-1933557350242575"
             data-ad-slot="2364594790"
             data-ad-format="auto"
-            data-full-width-responsive="true"></ins>
-         {/* // <div className="before:block before:pt-[66%] h-min mb-4 main !bg-transparent !p-0 relative overflow-hidden"> */}
-         {/* <ins
-            className="adsbygoogle"
-            style={{ display: "block" }}
-            data-ad-client="ca-pub-1933557350242575"
-            data-ad-slot="4986421263"
-            data-ad-format="auto"
-            data-full-width-responsive="true"
-            data-adtest="on"></ins>
-          */}
-
-         {/* <img
-            src="https://picsum.photos/300/200"
-            alt=""
-            className="absolute inset-0 w-full h-h-full object-cover "
-         /> */}
-         {/* // </div> */}
+            data-full-width-responsive="true"></ins> */}
+         <div className="before:block before:pt-[66%] h-min mb-4 main !bg-transparent !p-0 relative overflow-hidden">
+            <img
+               src="https://picsum.photos/300/200"
+               alt=""
+               className="absolute inset-0 w-full h-h-full object-cover "
+            />
+         </div>
       </>
    );
 };
