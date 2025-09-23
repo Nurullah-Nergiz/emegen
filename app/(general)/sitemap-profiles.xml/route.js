80b471e04bfdeecc3205ab2cc5e62c0405f64d23
Nurullah NERGİZ <nurullah.devs@gmail.com>
2025-09-23 23:01:11 +0300

    Refactor sitemap profile route to improve URL entry generation and XML conversion structure

--- app/(general)/sitemap-profiles.xml/route.js
+++ app/(general)/sitemap-profiles.xml/route.js
@@ -11,38 +11,60 @@ export async function GET() {
       encodeURI(`https://emgen.com.tr/@${userName}`);
 
    // Generate URL entries for the sitemap
-   const urlEntries = routes?.flatMap((route) => {
-      const lastmod = route?.updatedAt || new Date().toISOString();
+   // const urlEntries = routes?.flatMap((route) => {
+   //    const lastmod = route?.updatedAt || new Date().toISOString();
 
-      return [
-         { loc: generateProfileUrl(route.userName), lastmod, priority: 0.9 },
-         {
-            loc: generateProfileUrl(`${route.userName}/tenders`),
-            lastmod,
-            priority: 0.8,
+   //    return [
+   //       { loc: generateProfileUrl(route.userName), lastmod, priority: 0.9 },
+   //       {
+   //          loc: generateProfileUrl(`${route.userName}/tenders`),
+   //          lastmod,
+   //          priority: 0.8,
+   //       },
+   //       {
+   //          loc: generateProfileUrl(`${route.userName}/posts`),
+   //          lastmod,
+   //          priority: 0.8,
+   //       },
+   //    ];
+   // });
+
+   const sitemapObject = {
+      urlset: {
+         _attributes: {
+            xmlns: "http://www.sitemaps.org/schemas/sitemap/0.9",
          },
-         {
-            loc: generateProfileUrl(`${route.userName}/posts`),
-            lastmod,
-            priority: 0.8,
-         },
-      ];
-   });
+
+         url: routes?.flatMap((route) => {
+            const lastmod = route?.updatedAt || new Date().toISOString();
+
+            return [
+               {
+                  loc: generateProfileUrl(route.userName),
+                  lastmod,
+                  priority: 0.9,
+               },
+               {
+                  loc: generateProfileUrl(`${route.userName}/tenders`),
+                  lastmod,
+                  priority: 0.8,
+               },
+               {
+                  loc: generateProfileUrl(`${route.userName}/posts`),
+                  lastmod,
+                  priority: 0.8,
+               },
+            ];
+         }),
+      },
+   };
 
    // Convert URL entries to XML format
-   const sitemap = convert.js2xml(
-      {
-         urlset: {
-            urlset: {
-               _attributes: {
-                  xmlns: "http://www.sitemaps.org/schemas/sitemap/0.9",
-               },
-               url: urlEntries,
-            },
-         },
-      },
-      { compact: true, ignoreComment: true, spaces: 4 }
-   );
+   const sitemap = convert.js2xml(sitemapObject, {
+      compact: true,
+      ignoreComment: true,
+      spaces: 4,
+   });
 
    // Return the sitemap as a response
    return new Response(sitemap, {
