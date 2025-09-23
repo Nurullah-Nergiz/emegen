522769a1839fd3ee35dfdc1a0214f4f2a7077fd9
Nurullah NERGİZ <nurullah.devs@gmail.com>
2025-09-23 22:26:42 +0300

    Refactor sitemap generation to use xml-js for improved XML conversion

--- app/(general)/sitemaps.xml/route.js
+++ app/(general)/sitemaps.xml/route.js
@@ -1,41 +1,50 @@
-import js2xmlparser from "js2xmlparser";
+import convert from "xml-js";
 
 const baseUrl = process.env.NEXT_PUBLIC_BASE_URL || "https://emegen.com.tr";
 export function GET() {
+   console.clear();
+
    const sitemapObject = {
-      "@": {
-         xmlns: "http://www.sitemaps.org/schemas/sitemap/0.9",
+      urlset: {
+         _attributes: {
+            xmlns: "http://www.sitemaps.org/schemas/sitemap/0.9",
+         },
+
+         url: [
+            {
+               loc: `${baseUrl}/`,
+               lastmod: new Date().toISOString(),
+               priority: 0.8,
+            },
+            {
+               loc: `${baseUrl}/auth/forgot-password`,
+               lastmod: new Date().toISOString(),
+               priority: 0.8,
+            },
+            {
+               loc: `${baseUrl}/auth/register`,
+               lastmod: new Date().toISOString(),
+               priority: 0.8,
+            },
+            {
+               loc: `${baseUrl}/terms`,
+               lastmod: new Date().toISOString(),
+               priority: 0.8,
+            },
+            {
+               loc: `${baseUrl}/auth/login`,
+               lastmod: new Date().toISOString(),
+               priority: 0.8,
+            },
+         ],
       },
-      url: [
-         {
-            loc: `${baseUrl}/`,
-            lastmod: new Date().toISOString(),
-            priority: 0.8,
-         },
-         {
-            loc: `${baseUrl}/auth/forgot-password`,
-            lastmod: new Date().toISOString(),
-            priority: 0.8,
-         },
-         {
-            loc: `${baseUrl}/auth/register`,
-            lastmod: new Date().toISOString(),
-            priority: 0.8,
-         },
-         {
-            loc: `${baseUrl}/terms`,
-            lastmod: new Date().toISOString(),
-            priority: 0.8,
-         },
-         {
-            loc: `${baseUrl}/auth/login`,
-            lastmod: new Date().toISOString(),
-            priority: 0.8,
-         },
-      ],
    };
 
-   const sitemap = js2xmlparser.parse("urlset", sitemapObject);
+   const sitemap = convert.js2xml(sitemapObject, {
+      compact: true,
+      ignoreComment: true,
+      spaces: 4,
+   });
 
    return new Response(sitemap, {
       headers: {
