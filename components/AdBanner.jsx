6c0beaf2680a8e01277f3f71d2127c2af91678de
Nurullah NERGİZ <nurullah.devs@gmail.com>
2025-10-03 19:47:17 +0300

    Enhance AdBanner component to conditionally render ads on the client side and add ads.txt for ad verification

--- components/AdBanner.jsx
+++ components/AdBanner.jsx
@@ -1,38 +1,33 @@
 "use client";
 
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 export const Ad = () => {
-   // useEffect(() => {
-   //    if (typeof window !== "undefined" && window.adsbygoogle) {
-   //       try {
-   //          (window.adsbygoogle = window.adsbygoogle || []).push({});
-   //          // console.log(
-   //          //    "file: AdBanner.jsx:10 => window.adsbygoogle=>",
-   //          //    window.adsbygoogle
-   //          // );
-   //       } catch (e) {
-   //          // console.error("Adsense error:", e);
-   //       }
-   //    }
-   // }, []);
+   const [isClient, setIsClient] = useState(false);
+
+   useEffect(() => {
+      setIsClient(true);
+      if (typeof window !== "undefined" && window.adsbygoogle) {
+         try {
+            (window.adsbygoogle = window.adsbygoogle || []).push({});
+         } catch (e) {
+            console.error("Adsense error:", e);
+         }
+      }
+   }, []);
 
    return (
       <>
-         {/* <ins
-            className="adsbygoogle block min-h-52 !bg-secondary  main !p-0 relative overflow-hidden"
-            // style="display:block"
-            data-ad-client="ca-pub-1933557350242575"
-            data-ad-slot="2364594790"
-            data-ad-format="auto"
-            data-full-width-responsive="true"></ins> */}
-         <div className="before:block before:pt-[66%] h-min mb-4 main !bg-transparent !p-0 relative overflow-hidden">
-            <img
-               src="https://picsum.photos/300/200"
-               alt=""
-               className="absolute inset-0 w-full h-h-full object-cover "
-            />
-         </div>
+         {isClient && (
+            <div className="before:block before:pt-[66%] h-min mb-4 main !bg-transparent !p-0 relative overflow-hidden">
+               <ins
+                  className="adsbygoogle block min-h-52 !bg-secondary absolute inset-0 w-full overflow-hidden"
+                  data-ad-client="ca-pub-1933557350242575"
+                  data-ad-slot="2364594790"
+                  data-ad-format="auto"
+                  data-full-width-responsive="true"></ins>
+            </div>
+         )}
       </>
    );
 };
