5be2afce72775c753f509f9a0080d2316e4cd5b8
Nurullah NERGİZ <nurullah.devs@gmail.com>
2025-10-15 22:07:07 +0300

    Refactor CompleteProfile component to remove unused children prop and improve loading state handling

--- components/widgets/profile/completeProfile.jsx
+++ components/widgets/profile/completeProfile.jsx
@@ -1,6 +1,6 @@
 import ChartsDoughnut from "@/components/charts/doughnut";
 
-export default function CompleteProfile({ children, user = {} }) {
+export default function CompleteProfile({ user = {} }) {
    const userField =
       user &&
       Object.entries(user).filter(
@@ -20,7 +20,7 @@ export default function CompleteProfile({ children, user = {} }) {
                "isPhoneVerified",
                "isTwoFactorEnabled",
                "isBlocked",
-               "isverified",
+               "isVerified",
             ].includes(key)
       );
 
@@ -45,54 +45,71 @@ export default function CompleteProfile({ children, user = {} }) {
                "isPhoneVerified",
                "isTwoFactorEnabled",
                "isBlocked",
-               "isverified",
+               "isVerified",
             ].includes(key) && val
       ).length;
 
    return (
-      <>
+      <section className=" flex flex-col gap-4 p-4 ">
          <h2 className="text-xl text-center">Profilinizi tamamlayın</h2>
 
-         <ChartsDoughnut
-            datasets={[
-               {
-                  label: "",
-                  data: [
-                     100,
-                     Math.round(
-                        (100 / userField?.length) * validUserFieldCount
-                     ) ?? 0,
-                  ],
-                  backgroundColor: ["rgba(0, 0, 0, .12)", "#d81f26"],
-                  borderWidth: 1,
-               },
-            ]}
-         />
-         <ul className="">
-            {userField &&
-               userField.sort().map(([key, val]) => {
-                  const isComplete =
-                     val == null || val == undefined || val == ""
-                        ? false
-                        : true;
-                  return (
-                     <li key={key} className="py-2 flex items-center gap-2">
-                        <i
-                           className={`bx ${
-                              isComplete ? "bx-check !text-green-500" : "bx-x"
-                           } text-2xl`}></i>
-                        <span className={`${isComplete ? "font-bold" : ""}`}>
-                           {key.charAt(0).toUpperCase() +
-                              key
-                                 .replace(/([A-Z])/g, " $1")
-                                 .replace(/^./, (str) => str.toUpperCase())
-                                 .slice(1)}
-                        </span>
-                        {/* <span>{user[key]}</span> */}
-                     </li>
-                  );
-               })}
-         </ul>
-      </>
+         <div className="main">
+            {Object.keys(user || {}).length === 0 ? (
+               <p>Yükleniyor...</p>
+            ) : (
+               <>
+                  <ChartsDoughnut
+                     datasets={[
+                        {
+                           label: "",
+                           data: [
+                              100,
+                              Math.round(
+                                 (100 / userField?.length) * validUserFieldCount
+                              ) ?? 0,
+                           ],
+                           backgroundColor: ["rgba(0, 0, 0, .12)", "#d81f26"],
+                           borderWidth: 1,
+                        },
+                     ]}
+                  />
+                  <ul className="">
+                     {userField &&
+                        userField.sort().map(([key, val]) => {
+                           const isComplete =
+                              val == null || val == undefined || val == ""
+                                 ? false
+                                 : true;
+                           return (
+                              <li
+                                 key={key}
+                                 className="py-2 flex items-center gap-2">
+                                 <i
+                                    className={`bx ${
+                                       isComplete
+                                          ? "bx-check !text-green-500"
+                                          : "bx-x"
+                                    } text-2xl`}></i>
+                                 <span
+                                    className={`${
+                                       isComplete ? "font-bold" : ""
+                                    }`}>
+                                    {key.charAt(0).toUpperCase() +
+                                       key
+                                          .replace(/([A-Z])/g, " $1")
+                                          .replace(/^./, (str) =>
+                                             str.toUpperCase()
+                                          )
+                                          .slice(1)}
+                                 </span>
+                                 {/* <span>{user[key]}</span> */}
+                              </li>
+                           );
+                        })}
+                  </ul>
+               </>
+            )}
+         </div>
+      </section>
    );
 }
