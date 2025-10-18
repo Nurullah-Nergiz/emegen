b11cad02440bd7d44310b4c5e8e110e1a598aa10
Nurullah NERGİZ <nurullah.devs@gmail.com>
2025-10-18 17:42:59 +0300

    Add account settings page with user information editing functionality

--- app/(app)/settings/account/page.jsx
+++ app/(app)/settings/account/page.jsx
@@ -1,12 +1,176 @@
 "use client";
-import userContext  from "@/components/provider/userContext";
-import { use } from "react";
+import userContext from "@/components/provider/userContext";
+import { use, useEffect, useMemo, useState, useTransition } from "react";
+import { SecondaryBtn } from "@/components/btn";
+import { putUser } from "@/services/user";
 
 export default function AccountPage() {
    const [user, setUser] = use(userContext);
+   const [account, setAccount] = useState({
+      userName: user?.userName ?? "",
+      email: user?.email ?? "",
+      publicEmail: user?.publicEmail ?? "",
+      phoneNumber: user?.phoneNumber ?? "",
+      publicPhoneNumber: user?.publicPhoneNumber ?? "",
+   });
+   useEffect(() => {
+      setAccount({
+         userName: user?.userName ?? "",
+         email: user?.email ?? "",
+         publicEmail: user?.publicEmail ?? "",
+         phoneNumber: user?.phoneNumber ?? "",
+         publicPhoneNumber: user?.publicPhoneNumber ?? "",
+      });
+   }, [user]);
 
-   console.log("user from context in account page=>", user);
-   
+   if (!user) {
+      return (
+         <div className="p-4">
+            <h2 className="mb-2 text-2xl font-semibold">Hesap Ayarları</h2>
+            <p className="text-gray-600">Yükleniyor...</p>
+         </div>
+      );
+   }
 
-   return <>account page</>;
+   const handleChange = (field, value) => {
+      setAccount({
+         ...account,
+         [field]: value,
+      });
+   };
+
+   const handleSave = async (field, value) => {
+      putUser({
+         [field]: value,
+      })
+         .then((res) => {
+            if (res.status === 200) {
+               setUser({
+                  ...user,
+                  [field]: value,
+               });
+            } else {
+               console.error("Failed to update tags");
+            }
+         })
+         .catch((error) => {
+            console.error("Error updating tags:", error);
+         });
+   };
+
+   return (
+      <>
+         <main className="">
+            <h1 className="text-3xl font-semibold mb-6">Hesap Ayarları</h1>
+            <section className="main">
+               <ul className="max-w-3xl flex flex-col gap-6">
+                  <li className="w-full flex items-end gap-4">
+                     <label className="flex-1 flex flex-col  gap-4">
+                        <span className="font-medium">Kullanıcı Adı:</span>
+                        <input
+                           type="text"
+                           className="w-full h-9 px-3 py-2 !bg-transparent border relative border-tertiary shadow shadow-tertiary rounded-2xl outline-none"
+                           placeholder="Kullanıcı Adı"
+                           value={account?.userName ?? ""}
+                           onChange={(e) =>
+                              handleChange("userName", e.target.value)
+                           }
+                        />
+                     </label>
+                     <SecondaryBtn
+                        onClick={() =>
+                           handleSave("userName", account.userName)
+                        }>
+                        Kaydet
+                     </SecondaryBtn>
+                  </li>
+                  <li className="w-full flex items-end gap-4">
+                     <label className="flex-1 flex flex-col  gap-4">
+                        <span className="font-medium">E-posta:</span>
+                        <input
+                           type="email"
+                           className="w-full h-9 px-3 py-2 !bg-transparent border relative border-tertiary shadow shadow-tertiary rounded-2xl outline-none"
+                           placeholder="E-posta"
+                           value={account?.email ?? ""}
+                           onChange={(e) =>
+                              handleChange("email", e.target.value)
+                           }
+                        />
+                     </label>
+                     <SecondaryBtn
+                        onClick={() => handleSave("email", account.email)}>
+                        Kaydet
+                     </SecondaryBtn>
+                  </li>
+                  <li className="w-full flex items-end gap-4">
+                     <label className="flex-1 flex flex-col  gap-4">
+                        <span className="font-medium">Görünen E-posta:</span>
+                        <input
+                           type="email"
+                           className="w-full h-9 px-3 py-2 !bg-transparent border relative border-tertiary shadow shadow-tertiary rounded-2xl outline-none"
+                           placeholder="Görünen E-posta"
+                           value={account?.publicEmail ?? ""}
+                           onChange={(e) =>
+                              handleChange("publicEmail", e.target.value)
+                           }
+                        />
+                     </label>
+                     <SecondaryBtn
+                        onClick={() =>
+                           handleSave("publicEmail", account.publicEmail)
+                        }>
+                        Kaydet
+                     </SecondaryBtn>
+                  </li>
+
+                  <li className="w-full flex items-end gap-4">
+                     <label className="flex-1 flex flex-col  gap-4">
+                        <span className="font-medium">Telefon Numarası:</span>
+                        <input
+                           type="tel"
+                           className="w-full h-9 px-3 py-2 !bg-transparent border relative border-tertiary shadow shadow-tertiary rounded-2xl outline-none"
+                           placeholder="Telefon Numarası"
+                           value={account?.phoneNumber ?? ""}
+                           onChange={(e) =>
+                              handleChange("phoneNumber", e.target.value)
+                           }
+                        />
+                     </label>
+                     <SecondaryBtn
+                        onClick={() =>
+                           handleSave("phoneNumber", account.phoneNumber)
+                        }>
+                        Kaydet
+                     </SecondaryBtn>
+                  </li>
+                  <li className="w-full flex items-end gap-4">
+                     <label className="flex-1 flex flex-col gap-4">
+                        <span className="font-medium">
+                           Görünen Telefon Numarası:
+                        </span>
+                        <input
+                           type="tel"
+                           className="w-full h-9 px-3 py-2 !bg-transparent border relative border-tertiary shadow shadow-tertiary rounded-2xl outline-none"
+                           placeholder="Görünen Telefon Numarası"
+                           value={user.publicPhoneNumber ?? ""}
+                           onChange={(e) =>
+                              handleChange("publicPhoneNumber", e.target.value)
+                           }
+                        />
+                     </label>
+                     <SecondaryBtn
+                        onClick={() =>
+                           handleSave(
+                              "publicPhoneNumber",
+                              account.publicPhoneNumber
+                           )
+                        }>
+                        Kaydet
+                     </SecondaryBtn>
+                  </li>
+               </ul>
+            </section>
+         </main>
+      </>
+   );
 }
