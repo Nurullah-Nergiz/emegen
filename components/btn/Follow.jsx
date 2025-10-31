8e8ef6e84ab7f6b621c125bcf450b5b2a13d16d4
Nurullah NERGİZ <nurullah.devs@gmail.com>
2025-10-31 21:57:11 +0300

    Implement follow/unfollow functionality in FollowBtn component

--- components/btn/Follow.jsx
+++ components/btn/Follow.jsx
@@ -2,28 +2,47 @@
 
 import { useState } from "react";
 import { SecondaryBtn } from "./index";
-import { setUserFallowing } from "@/services/userFallow";
+import { setUserFallowing, setUserUnFallowing } from "@/services/userFallow";
 
-export default function FollowBtn({ id = "",isFollowing=false, className = "", ...props }) {
+export default function FollowBtn({
+   id = "",
+   isFollowing = false,
+   className = "",
+   ...props
+}) {
    const [userFollow, setUserFollow] = useState(isFollowing);
 
    return (
       <SecondaryBtn
          className={`${className} whitespace-nowrap`}
          onClick={() => {
-            console.log(`Following user with ID: ${id}`);
-            setUserFallowing(id)
-               .then((res) => {
-                  if (res.status === 200) {
-                     console.log("User followed successfully");
-                     setUserFollow(true);
-                  } else {
-                     console.error("Failed to follow user");
-                  }
-               })
-               .catch((err) => {
-                  console.error("Error following user:", err);
-               });
+            if (!userFollow) {
+               setUserFallowing(id)
+                  .then((res) => {
+                     if (res.status === 201) {
+                        console.log("User followed successfully");
+                        setUserFollow(true);
+                     } else {
+                        console.error("Failed to follow user");
+                     }
+                  })
+                  .catch((err) => {
+                     console.error("Error following user:", err);
+                  });
+            } else {
+               setUserUnFallowing(id)
+                  .then((res) => {
+                     if (res.status === 200) {
+                        console.log("User unfollowed successfully");
+                        setUserFollow(false);
+                     } else {
+                        console.error("Failed to unfollow user");
+                     }
+                  })
+                  .catch((err) => {
+                     console.error("Error unfollowing user:", err);
+                  });
+            }
          }}
          {...props}>
          {userFollow ? "Takip bırak" : "Takip et"}
