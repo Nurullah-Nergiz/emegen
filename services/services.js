79830aabc93400e45a70146dd9174d919ddf6214
Nurullah NERGİZ <nurullah.devs@gmail.com>
2025-11-17 20:20:11 +0300

    Add service retrieval and creation functions with error handling

--- services/services.js
+++ services/services.js
@@ -1,4 +1,32 @@
 const { serviceInstance } = require(".");
 
-export const getUserServices = async (username) =>
-   serviceInstance.get(`/users/${username}/services`);
+export const getUserServices = async (username) => {
+   try {
+      return await serviceInstance.get(`/users/${username}/services`);
+   } catch (error) {
+      if (error.response?.status === 404) {
+         return { data: [], status: 404 };
+      }
+      // throw error;
+   }
+};
+
+export const getServiceById = async (username, serviceId) => {
+   try {
+      return await serviceInstance.get(
+         `/users/${username}/services/${serviceId}`
+      );
+   } catch (error) {
+      if (error.response?.status === 404) {
+         return { data: null, status: 404 };
+      }
+      // throw error;
+   }
+};
+
+export const createUserService = async ( serviceData) => {
+   return await serviceInstance.post(
+      `/services`,
+      serviceData
+   );
+};
