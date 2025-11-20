11ba7c5d4d3e6a4a288bc4e9ccc89b29890be3a6
Nurullah NERGİZ <nurullah.devs@gmail.com>
2025-11-20 19:52:45 +0300

    Add profile info header component with user details and actions

--- components/profile/header/index.jsx
+++ components/profile/header/index.jsx
@@ -13,125 +13,103 @@ export default function ProfileInfoHeader({
 
    return (
       <>
-         <header className="flex flex-col gap-4">
+         <header className="main !px-0 !pt-0 flex flex-col gap-4 overflow-hidden">
             <CoverImage src={user?.coverPicture} />
-
-            <section className="main py-0 flex flex-col sm:flex-row items-center gap-2 overflow-hidden">
+            <section className="flex items-start justify-between gap-4 px-4 -mt-16 sm:-mt-24 z-10">
                <AvatarImg
                   src={user?.profilePicture}
-                  className="w-auto h-full max-w-52 max-h-52 p-1 "
+                  className="w-auto h-full max-w-32 max-h-32 sm:max-w-48 sm:max-h-48 p-1 bg-background rounded-full border-2"
                   size={128 * 4}
                />
-               <div className="w-full flex flex-col  gap-2">
-                  <div className="flex flex-col items-center sm:items-start gap-2">
-                     <h1 className="inline-flex flex-col sm:flex-row items-center flex-wrap gap-0 text-xls font-bolds whitespace-nowrap">
-                        <span className="flex gap-2 items-center text-2xl font-bold">
-                           {user?.name}
-                           {user?.isVerified && (
-                              <i className="bx bxs-check-circle text-primary"></i>
-                           )}
-                        </span>
-                        <p className="text-accent text-base">
-                           @{user?.userName}
-                        </p>
-                     </h1>
-                     <h2 className="overflow-hidden whitespace-pre-line text-ellipsis text-sm">
-                        {user?.bio || "Bu kullanıcı hakkında bilgi yok."}
-                     </h2>
-                     <div className="flex gap-1">
-                        <Link
-                           href={`/@${cleanUsername}/followers`}
-                           className="">
-                           <b className="">{`${user?.followersCount ?? 0} `}</b>
-                           takipci
+               <div className="pt-16 sm:pt-28 flex items-center gap-4">
+                  {isAuthenticatedUser ? (
+                     <>
+                        <SecondaryBtn className="bx bx-share-alt !hidden sm:!flex py-2 px-4">
+                           Paylaş
+                        </SecondaryBtn>
+                        <Link href="/settings/edit-profile" className="">
+                           <PrimaryBtn className="bx bx-edit py-2 px-4">
+                              Profili Düzenle
+                           </PrimaryBtn>
                         </Link>
-                        <Link
-                           href={`/@${cleanUsername}/following`}
-                           className="">
-                           <b className="">{` - ${
-                              user?.followingCount ?? 0
-                           } `}</b>
-                           takip
-                        </Link>
-                        <span>
-                           <b className="">{` - ${user?.postCount ?? 0} `}</b>
-                           gönderi
-                        </span>
-                     </div>
-                     <h3 className="font-semibold">
-                        <ul className="flex items-center gap0">
-                           {user?.tags &&
-                              (typeof user?.tags[0] === "object"
-                                 ? user.tags[0]
-                                 : user.tags
-                              )?.map((tag) => (
-                                 <li
-                                    key={tag}
-                                    className="w-min px-2 py-1 rounded-md bg-accent text-sm underline">
-                                    #{tag}
-                                 </li>
-                              ))}
-                        </ul>
-                     </h3>
-
-                     {user?.location && (
-                        <div className="flex flex-wrap items-center">
-                           <>
-                              <i className="bx bx-map"></i>
-                              <p className=" px-1 text-base whitespace-nowrap">
-                                 {formatLocation(user?.location)}
-                              </p>
-                           </>
-                        </div>
-                     )}
-                  </div>
-                  <div className="w-full flex flex-col sm:flex-row justify-end items-center  gap-4">
-                     {isAuthenticatedUser ? (
-                        <>
-                           <SecondaryBtn className="bx bx-share-alt !w-full py-2 px-4">
-                              Paylaş
-                           </SecondaryBtn>
-                           <Link
-                              href="/settings/edit-profile"
-                              className="!w-full">
-                              <PrimaryBtn className="bx bx-edit !w-full py-2 px-4">
-                                 Profili Düzenle
-                              </PrimaryBtn>
+                     </>
+                  ) : (
+                     <>
+                        <FollowBtn
+                           id={user._id}
+                           isFollowing={user?.isFollowing}
+                           type="secondary"
+                           className=""
+                        />
+                        {user.primaryBtn?.type === "price" ||
+                        user.primaryBtn?.type === "" ? (
+                           <Link href={`/tenders/request/${user?._id}/`}>
+                              <PrimaryBtn>Fiyat Teklif İste</PrimaryBtn>
                            </Link>
-                        </>
-                     ) : (
-                        <>
-                           <FollowBtn
-                              id={user._id}
-                              isFollowing={user?.isFollowing}
-                              type="secondary"
-                              className="!w-full"
-                           />
-                           {user.primaryBtn?.type === "price" ? (
-                              <Link
-                                 className="!w-full"
-                                 href={`/tenders/request/${user?._id}/`}>
-                                 <PrimaryBtn className="!w-full">
-                                    Fiyat Teklif İste
-                                 </PrimaryBtn>
-                              </Link>
-                           ) : user.primaryBtn?.type === "custom" ? (
-                              <Link
-                                 className="!w-full"
-                                 href={user.primaryBtn?.url}
-                                 target="_blank"
-                                 rel="noopener noreferrer">
-                                 <PrimaryBtn className="!w-full">
-                                    {user.primaryBtn.text}
-                                 </PrimaryBtn>
-                              </Link>
-                           ) : (
-                              ""
-                           )}
-                        </>
-                     )}
-                  </div>
+                        ) : user.primaryBtn?.type === "custom" ? (
+                           <Link
+                              href={user.primaryBtn?.url}
+                              target="_blank"
+                              rel="noopener noreferrer">
+                              <PrimaryBtn>{user.primaryBtn.text}</PrimaryBtn>
+                           </Link>
+                        ) : (
+                           ""
+                        )}
+                     </>
+                  )}
                </div>
+            </section>
+            <section className="flex flex-col gap-4 px-4 -mt-4">
+               <div className="flex flex-col">
+                  <h1 className="inline-flex items-center gap-2 text-2xl font-bold">
+                     {user?.name}
+                     {user?.isVerified && (
+                        <i className="bx bxs-check-circle text-primary"></i>
+                     )}
+                  </h1>
+                  <p className="text-tertiary text-base">@{user?.userName}</p>
+               </div>
+
+               <h2 className="overflow-hidden whitespace-pre-line text-ellipsis text-sm">
+                  {user?.bio || "Bu kullanıcı hakkında bilgi yok."}
+               </h2>
+
+               <div className="flex gap-4">
+                  <Link href={`/@${cleanUsername}/followers`} className="">
+                     <b className="">{`${user?.followersCount ?? 0} `}</b>
+                     takipci
+                  </Link>
+                  <Link href={`/@${cleanUsername}/following`} className="">
+                     <b className="">{`${user?.followingCount ?? 0} `}</b>
+                     takip
+                  </Link>
+                  <span>
+                     <b className="">{`${user?.postCount ?? 0} `}</b>
+                     gönderi
+                  </span>
+               </div>
+
+               <ul className="flex items-center gap-2 font-semibold flex-wrap">
+                  {user?.tags &&
+                     (typeof user?.tags[0] === "object"
+                        ? user.tags[0]
+                        : user.tags
+                     )?.map((tag) => (
+                        <li
+                           key={tag}
+                           className="w-min px-2 py-1 rounded-md bg-accent text-sm underline">
+                           #{tag}
+                        </li>
+                     ))}
+               </ul>
+               {isAuthenticatedUser && (
+                  <div className="w-full flex sm:hidden flex-col sm:flex-row justify-end items-center  gap-4">
+                     <SecondaryBtn className="bx bx-share-alt !w-full py-2 px-4">
+                        Paylaş
+                     </SecondaryBtn>
+                  </div>
+               )}
             </section>
          </header>
       </>
