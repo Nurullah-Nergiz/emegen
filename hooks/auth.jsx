d3fe2bd0e0fa998ea58261b77e604d2574771480
Nurullah NERGİZ <nurullah.devs@gmail.com>
2025-11-27 18:44:54 +0300

    Refactor useAuthUser hook: return user object directly from cookie value

--- hooks/auth.jsx
+++ hooks/auth.jsx
@@ -5,7 +5,7 @@ import { cookies } from "next/headers";
 export default async function useAuthUser() {
    const cookieStore = await cookies();
    if (!cookieStore.get("user")?.value) return {};
-   else return JSON.parse(cookieStore.get("user")?.value);
+   else return JSON.parse(cookieStore.get("user")?.value)?.user;
 }
 
 export async function useAuthToken() {
