a6b2f168246fa3cbb2b808c51cf46161b417d748
Nurullah NERGİZ <nurullah.devs@gmail.com>
2025-12-30 19:11:56 +0300

    Add business type selection to EditProfilePage for user customization

--- app/(app)/settings/edit-profile/page.jsx
+++ app/(app)/settings/edit-profile/page.jsx
@@ -166,7 +166,83 @@ export default function EditProfilePage() {
                </div>
 
                <PrimaryFormButton />
-                                    
+
+               <div className="main">
+                  {/* buisiness type */}
+                  <div className="mb-4">
+                     <label
+                        htmlFor="businessType"
+                        className="block mb-2 font-medium">
+                        İşletme Türü
+                     </label>
+                     <select
+                        id="businessType"
+                        name="businessType"
+                        defaultValue={user?.businessType || ""}
+                        className="w-full text-black border border-gray-300 rounded-md p-2"
+                        onChange={(e) =>
+                           setUser((prev) => ({
+                              ...prev,
+                              businessType: e.target.value,
+                           }))
+                        }>
+                        {[
+                           "LocalBusiness",
+                           "ConstructionBusiness", //
+                           "HomeAndConstructionBusiness",
+                           "ProfessionalService",
+                           "Restaurant",
+                           "CafeOrCoffeeShop",
+                           "Bakery",
+                           "FoodEstablishment",
+                           "LegalService",
+                           "AccountingService",
+                           "RealEstateAgent",
+                           "AutoRepair",
+                           "AutoDealer",
+                           "GasStation",
+                           "Store",
+                           "ClothingStore",
+                           "ElectronicsStore",
+                           "GroceryStore",
+                           "GeneralContractor",
+                           "Electrician",
+                           "Plumber",
+                           "HVACBusiness",
+                           "Locksmith",
+                           "CleaningService",
+                           "MovingCompany",
+                           "LandscapeService",
+                           "HairSalon",
+                           "BeautySalon",
+                           "NailSalon",
+                           "DaySpa",
+                           "HealthClub",
+                           "MedicalBusiness",
+                           "Dentist",
+                           "CafeOrCoffeeShop",
+                           "BarOrPub",
+                           "FastFoodRestaurant",
+                           "Attorney",
+                           "AccountingService",
+                           "InsuranceAgency",
+                           "AutoDealer",
+                           "School",
+                           "ChildCare",
+                           "TravelAgency",
+                           "PetStore",
+                           "DryCleaningOrLaundry",
+                        ].map((type, i) => (
+                           <option
+                              key={`buisiness-type${type}-${i}`}
+                              value={type}>
+                              {type}
+                           </option>
+                        ))}
+                     </select>
+                  </div>
+               </div>
+
                <FormsWebsite
                   websites={user?.websites || {}}
                   onUpdated={handleWebsitesUpdated}
