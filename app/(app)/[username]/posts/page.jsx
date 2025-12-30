e12341712504c89a50457a53ca2551edf438d72b
Nurullah NERGİZ <nurullah.devs@gmail.com>
2025-12-30 19:11:31 +0300

    Add BreadcrumbSchema to user posts page for enhanced navigation

--- app/(app)/[username]/posts/page.jsx
+++ app/(app)/[username]/posts/page.jsx
@@ -1,6 +1,7 @@
 import { getUserPosts } from "@/services/user";
 import Posts from "@/components/widgets/post";
 import { cleanUserName } from "@/utils/user";
+import { BreadcrumbSchema } from "@/components/schema";
 
 export async function generateMetadata({ params }) {
    const { username } = await params;
@@ -22,23 +23,38 @@ export async function generateMetadata({ params }) {
 export default async function Page({ params }) {
    console.clear();
    const { username } = await params;
+   const cleanUsername = cleanUserName(username);
+   
    /**
     * @type {object {status: number, data: {posts: Array,limit: number,page: number,totalPages: number,totalPosts: number}}}
     */
    const { status, data: fetchedPosts } = await getUserPosts(
-      username.replace("%40", "").trim()
+      cleanUsername,
+      // { page: 1, limit: 10 }
    );
    console.clear();
    // console.log("file: page.jsx:7 => posts=>", fetchedPosts);
 
    return (
       <section className="">
-         <h1 className="py-2">Posts</h1>
+         <h4 className="py-2">Posts</h4>
          <Posts posts={fetchedPosts.posts} />
          {/* {posts?.map((post) => {
             return <Post key={post._id} post={post} />;
          })} */}
          {/* <Post /> */}
+         <BreadcrumbSchema
+            items={[
+               {
+                  name: `${cleanUsername}` || "Profile-posts",
+                  url: `/@${cleanUsername}`,
+               },
+               {
+                  name: "Posts",
+                  url: `/@${cleanUsername}/posts`,
+               },
+            ]}
+         />
       </section>
    );
 }
