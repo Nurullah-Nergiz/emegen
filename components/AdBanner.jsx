d097d80573679d13f0ff163bb142fb5a7955dee7
Nurullah NERGİZ <nurullah.devs@gmail.com>
2025-12-30 21:58:00 +0300

    Refactor AdBanner component to enable ad rendering logic and clean up commented code

--- components/AdBanner.jsx
+++ components/AdBanner.jsx
@@ -1,24 +1,24 @@
 "use client";
 
-// import { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 
 export const Ad = () => {
-   // const [isClient, setIsClient] = useState(false);
+   const [isClient, setIsClient] = useState(false);
 
-   // useEffect(() => {
-   //    setIsClient(true);
-   //    if (typeof window !== "undefined" && window.adsbygoogle) {
-   //       try {
-   //          (window.adsbygoogle = window.adsbygoogle || []).push({});
-   //       } catch (e) {
-   //          console.error("Adsense error:", e);
-   //       }
-   //    }
-   // }, []);
+   useEffect(() => {
+      setIsClient(true);
+      if (typeof window !== "undefined" && window.adsbygoogle) {
+         try {
+            (window.adsbygoogle = window.adsbygoogle || []).push({});
+         } catch (e) {
+            console.error("Adsense error:", e);
+         }
+      }
+   }, []);
 
    return (
       <>
-         {/* {isClient && (
+         {isClient && (
             <div className="before:block before:pt-[66%] h-min mb-4 main !bg-transparent !p-0 relative overflow-hidden">
                <ins
                   className="adsbygoogle block min-h-52 !bg-secondary absolute inset-0 w-full overflow-hidden"
@@ -27,7 +27,7 @@ export const Ad = () => {
                   data-ad-format="auto"
                   data-full-width-responsive="true"></ins>
             </div>
-         )} */}
+         )}
       </>
    );
 };
