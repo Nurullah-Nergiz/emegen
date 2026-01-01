25feb26b5b40e9fc77f04bdb3dd0fa0ab9e6973b
Nurullah NERGİZ <nurullah.devs@gmail.com>
2026-01-01 18:36:55 +0300

    Refactor user service functions to remove caching from getUser and set default parameter for putUser

--- services/user.js
+++ services/user.js
@@ -1,11 +1,10 @@
-import { cache } from "react";
 import { userInstance } from "./index";
 
 /**
  *
  * @param {String} userName
  */
-export const getUser = cache((userName) => userInstance("users/" + userName));
+export const getUser = (userName) => userInstance("users/" + userName);
 
 export const getUsers = () => userInstance("users/");
 
@@ -22,7 +21,9 @@ export const getUserPosts = (userName) =>
 export const getUserTenders = (userName) =>
    userInstance(`users/${userName}/tenders`);
 
-export const putUser = (data) => userInstance.put(`users/me`, data);
+
+
+export const putUser = (data={}) => userInstance.put(`users/me`, data);
 
 export const putUserAvatar = (data) =>
    userInstance.put(`users/me/avatar`, data, {
