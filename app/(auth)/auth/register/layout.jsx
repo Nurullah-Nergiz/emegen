13cceed92ed218fb36b4a97e23ae11d6d91567e7
Nurullah NERGİZ <nurullah.devs@gmail.com>
2026-01-03 19:26:08 +0300

    Update registration layout metadata for improved clarity and accuracy

--- app/(auth)/auth/register/layout.jsx
+++ app/(auth)/auth/register/layout.jsx
@@ -1,15 +1,15 @@
-
 export default function RootLayout({ children }) {
    return <>{children}</>;
 }
 
 export const metadata = {
    title: "Kayıt Ol",
-   description: "Emegen\’e ücretsiz üye olun. Avantajlardan yararlanın, işlemlerinizi kolayca yönetin.",
+   description:
+      "Emegen’e ücretsiz üye olun. Avantajlardan yararlanın, işlemlerinizi kolayca yönetin.",
    keywords:
       "Emegen kayıt, kullanıcı kaydı, üye kaydı, hesap kaydı, kayıt ol, ücretsiz kayıt",
    alternates: {
-      canonical:"https://emegen.com.tr/auth/login/auth/register",
+      canonical: "https://emegen.com.tr/auth/register",
    },
 };
 export const dynamic = "force-dynamic"; // Force dynamic rendering for this layout
